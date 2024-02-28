41ffc9685828ae3a8875beb3b6baabd6b0404c0a
codeforme1234 <deveshkumar_21294@aitpune.edu.in>
2024-02-28 23:35:31 +0530

    hero section done

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import ContactInfo from './Contactinfo'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import ContactInfo from "./Contactinfo";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-
-    <ContactInfo/>
-        
-    </>
-  )
+      <div className="container-fluid">
+        <div className="heroSection">
+          <div className="mb-3 img-name">
+            <div className="imgg me-4" >
+              <img width="60" src="src\assets\react.svg" alt="bjsdb" />
+            </div>
+            <div className="name-dev">
+              <div className="t1">Devesh</div>
+              <div className="t2">Developer at Barclays london</div>
+            </div>
+          </div>
+          <div className="img-name-body">
+            Welcome to my digital garden where I share what I'm learning 
+            <br/>
+            about shipping great products, becoming a better developer and
+            <br/>
+            growing a carrer in tech.
+          </div>
+        </div>
+      </div>
+  );
 }
 
-export default App
+export default App;
