1bb0347a18482a3a8e1f578fe720fbc79f14d2bc
nasablackberry <singhshivank0605@gmail.com>
2024-02-28 22:42:20 +0530

    Ist

---
+++ src/Contactinfo.jsx
@@ -0,0 +1,14 @@
+// ContactInfo.js
+import React from 'react';
+
+function ContactInfo(){
+    return(
+        <>
+        <div className="container">
+            <h1>Shivank Singh</h1>
+        </div>
+        </>
+    )
+}
+
+export default ContactInfo;
