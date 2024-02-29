f8a8d638955eb24bec2a0f2f31409e96f0b567e3
codeforme1234 <deveshkumar_21294@aitpune.edu.in>
2024-03-01 00:20:30 +0530

    latest commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,25 +4,43 @@ import bgimage from "./assets/bg.jpg";
 import ContactInfo from "./Contactinfo";
 import "./App.css";
 import Navbar from "./navbar";
+import React, { useEffect, useRef } from 'react';
 
 function App() {
   const [count, setCount] = useState(0);
+  // const cardRef = useRef(null);
+
+  // useEffect(() => {
+  //   const handleMouseMove = (e) => {
+  //     const cardRect = cardRef.current.getBoundingClientRect();
+  //     const centerX = cardRect.left + cardRect.width / 2;
+  //     const centerY = cardRect.top + cardRect.height / 2;
+  //     let x = e.pageX - centerX;
+  //     let y = e.pageY - centerY;
+  //     const transformValue = `translate(${x / 40}px, ${y / 40}px)`;
+  //     cardRef.current.style.transform = transformValue;
+  //   };
+  //   window.addEventListener("mousemove", handleMouseMove);
+  //   return () => {
+  //     window.removeEventListener("mousemove", handleMouseMove);
+  //   };
+  // }, []);
 
   return (
     <div className="container-fluid">
       <div className="heroSection">
         <div className="img-name">
           <div className="imgg">
-            <img width="80" src="src\assets\profile-image.png" alt="bjsdb" />
+            <img width="100" src="src\assets\profile-image.png" alt="bjsdb" />
           </div>
           <div className="name-dev">
             <div className="t1">Devesh</div>
-            <div className="t2">Frontend Devloper | UI designer</div>
+            <div className="t2">Frontend Developer | UI designer</div>
           </div>
         </div>
         <div className="img-name-body">
           Welcome to my digital garden where I share what I'm learning about
-          shipping great products, becoming a better developer and growing a
+          creating great products, becoming a better developer and growing a
           carrer in tech.
         </div>
       </div>
